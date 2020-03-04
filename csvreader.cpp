39a1eada521e3a809ea58eb1073f68d3308ff7a9
rikandart <pizhun97@gmail.com>
2020-03-04 19:34:19 +0300

    дополнение

--- csvreader.cpp
+++ csvreader.cpp
@@ -1,7 +1,3 @@
-// csv_parser.cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -10,24 +6,39 @@
 #include <vector>
 #include <map>
 
-#define _DEBUG_USE
-//#define _CONSOLE_USE
+//#define _DEBUG_USE
+#define _CONSOLE_USE
 
 using namespace std;
 
-vector<vector<string>> table;// вектор таблицы
-// карта, запоминающая индексы выражений для того, чтобы не перебирать всю таблицу
-// т.к. таблица может быть большой, этот вектор ускорит поиск
-// номер строки -> столбец
-multimap<int, int> equals_indexes;
-map<string, int> columns;// карта названия колонок
-// ряд -> индекс в массиве
-map<string, int> rows;// карта рядов
+class Parser {
+public:
+	Parser(){
+		opers_array[0] = '+';
+		opers_array[1] = '-';
+		opers_array[2] = '*';
+		opers_array[3] = '/';
+	};
+	~Parser(){};
+	vector<vector<string>> table;// вектор таблицы
+	// карта, запоминающая индексы выражений для того, чтобы не перебирать всю таблицу
+	// т.к. таблица может быть большой, этот вектор ускорит поиск
+	// номер строки -> столбец
+	multimap<int, int> equals_indexes;
+	map<string, int> columns;// карта названия колонок
+	// ряд -> индекс в массиве
+	map<string, int> rows;// карта рядов
+	void watch_table();
+	void print_table();
+private:
+	// массив операторов
+	char opers_array[4];
+	int operation(int arg1, int arg2, char oper);
+	int parse_arg(string* arg);
+	string parse_equal(const string* str, const unsigned int row, const unsigned int column);
+};
 
-// массив операторов
-const char opers_array[] = { '+', '-', '*', '/' };
-
-int operation(int arg1, int arg2, char oper){
+int Parser::operation(int arg1, int arg2, char oper){
 	switch (oper){
 		case '+':
 			return arg1 + arg2;
@@ -40,9 +51,11 @@ int operation(int arg1, int arg2, char oper){
 	}
 }
 
-int parse_arg(string* arg){
+int Parser::parse_arg(string* arg){
 	string column = "";
 	string row = "";
+	if (arg->at(0) == '=')
+		*arg = arg->substr(1, arg->length());// убираем "="
 	const char* arg_c = arg->c_str();
 	for (char &i : *arg){
 		if (!isdigit(i)){
@@ -51,23 +64,30 @@ int parse_arg(string* arg){
 			row += i;
 		}
 	}
-	int i_col = columns.find(column)->second;
-	int i_row = rows.find(row)->second;
+	if (column != ""){
+		int i_col = columns.find(column)->second;
+		int i_row = rows.find(row)->second;
 
-	return stoi(table[i_row][i_col]);
+		if (table[i_row][i_col].at(0) == '=')
+			table[i_row][i_col] = parse_equal(&table[i_row][i_col], i_row, i_col);
+
+		return stoi(table[i_row][i_col]);
+	} else {// если нет букв в column, значит, это просто число
+		return stoi(row);
+	}
+	
 }
 
 // парсер выражений
-string parse_equal(string str, const unsigned int row, const unsigned int column){
-	str = str.substr(1, str.length());// убираем "="
+string Parser::parse_equal(const string* str, const unsigned int row, const unsigned int column){
 	int cell_1 = 0, cell_2 = 0;
 	char op;
 	for (char oper : opers_array){
 		int oper_pos = 0;
-		if ((oper_pos = str.find_first_of(oper, 0)) >= 0){
-			string arg1 = str.substr(0, oper_pos);// получаем первый аргумент выражения
+		if ((oper_pos = str->find_first_of(oper, 0)) >= 0){
+			string arg1 = str->substr(0, oper_pos);// получаем первый аргумент выражения
 			cell_1 = parse_arg(&arg1);// получаем значение из ячейки
-			string arg2 = str.substr(oper_pos + 1, str.length());// получаем второй аргумент выражения
+			string arg2 = str->substr(oper_pos + 1, str->length());// получаем второй аргумент выражения
 			cell_2 = parse_arg(&arg2);// получаем значение из ячейки
 			op = oper;
 			break;
@@ -77,16 +97,17 @@ string parse_equal(string str, const unsigned int row, const unsigned int column
 }
 
 // перебор ячеек
-void watch_table() {
+void Parser::watch_table() {
 	for (map<int, int>::iterator it = equals_indexes.begin(); it != equals_indexes.end(); it++){
 		unsigned int row = it->first;
 		unsigned int column = it->second;
-		table[row][column] = parse_equal(table[row][column], row, column);
+		if (table[row][column].at(0) == '=')
+			table[row][column] = parse_equal(&table[row][column], row, column);
 	}
 }
 
 // печать таблицы в консоль
-void print_table() {
+void Parser::print_table() {
 
 	for (map<string, int>::iterator it = columns.begin(); it != columns.end(); it++){
 		cout << it->first;
@@ -118,6 +139,7 @@ int main(int argc, char* argv[]){
 	if (argc > 0 && argv[1]){
 		filename = argv[1];
 	}
+	Parser p;
 	char input[1024] = "";
 	string row = "", cell = "";
 	fstream filehandle(filename.c_str(), ios::in);
@@ -137,32 +159,32 @@ int main(int argc, char* argv[]){
 			if (column_read){
 				int column_i = 0; // считываем номер колонки (столбца) в карту колонок
 				while (getline(ss_row, cell, ',')){
-					columns.insert(pair<string, int>(cell, column_i++));
+					p.columns.insert(pair<string, int>(cell, column_i++));
 				}
 				column_read = false;
 			}
 			// тут заполняется сама таблица
 			else{
 				vector<string> row_vec;
-				table.push_back(row_vec);
-				int i = table.size() - 1;// номер строки
+				p.table.push_back(row_vec);
+				int i = p.table.size() - 1;// номер строки
 				bool row_read = true;
 				while (getline(ss_row, cell, ',')){
 					// считываем номер ряда в карту рядов
 					if (row_read){
-						rows.insert(pair<string, int>(cell, row_i++));
+						p.rows.insert(pair<string, int>(cell, row_i++));
 						row_read = false;
 					}
 
-					table[i].push_back(cell);
+					p.table[i].push_back(cell);
 					// если выражение -> запоминаем строку и столбец
 					if (cell.at(0) == '=')
-						equals_indexes.insert(pair<int, int>(i, table[i].size() - 1));
+						p.equals_indexes.insert(pair<int, int>(i, p.table[i].size() - 1));
 				}
 			}
 		}
-		watch_table();
-		print_table();
+		p.watch_table();
+		p.print_table();
 		system("pause");
 	}
 	
